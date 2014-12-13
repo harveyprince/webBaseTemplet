8d317bb5961a5880eaf5fdd8ea8f0ad0fea27eea
harveyprince <948841233@qq.com>
2014-12-14 01:27:16 +0800

    login signup switch

--- Public/js/welcome/welcome.js
+++ Public/js/welcome/welcome.js
@@ -1,2 +1,8 @@
+$(".switch-button").click(function(){
+	var $tohide = $(".input-group:visible");
+	var $toshow = $(".input-group:hidden");
+	$tohide.hide();
+	$toshow.fadeIn("normal");
+});
 $('.box-button').click(function(){
 });
