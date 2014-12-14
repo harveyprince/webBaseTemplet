f1fec07a0f2fbe9a0fc9c55f26565c4532976fb3
harveyprince <948841233@qq.com>
2014-12-14 19:07:05 +0800

    two kinds of form submit ways

--- Public/js/welcome/welcome.js
+++ Public/js/welcome/welcome.js
@@ -4,12 +4,14 @@ $(".switch-button").click(function(){
 	$tohide.hide();
 	$toshow.fadeIn("normal");
 });
+//submit:use formdata
 $("form").submit(function(e){
+	var $box = $(this).find('.alarm-box').alarmInit();
+	var action = $(this).attr('action');
 	var data = new FormData();
 	$(this).find('input').each(function(){
 		data.append($(this).attr('name'),$(this).val());
 	});
-	var action = $(this).attr('action');
 	$.ajax({
 		data: data,
 		type: "POST",
@@ -18,13 +20,35 @@ $("form").submit(function(e){
 		contentType: false,
 		processData: false,
 		success: function(data) {
-			alert(data);
+			//callback code
 		},
 		error:function(){
+			$box.alarm("error occured");
 		}
 	});
 	e.preventDefault();
 });
+//submit:without formdata
+/*
+$("form").submit(function(e){
+	var $box = $(this).find('.alarm-box').alarmInit();
+	var action = $(this).attr('action');
+	var email = $(this).find('input[name="email"]').val();
+	var password = $(this).find('input[name="password"]').val();
+	if($(this).attr('type')=='login'){
+		$.post(action,{email:email,password:password},function(data,status){
+			//callback code
+		});
+	}
+	if($(this).attr('type')=='signup'){
+		var surname = $(this).find('input[name="surname"]').val();
+		var psname = $(this).find('input[name="psname"]').val();
+		$.post(action,{email:email,password:password,surname:surname,psname:psname},function(data,status){
+			//callback code
+		});
+	}
+});
+*/
 function validate_email(field)
 {
 	with (field)
